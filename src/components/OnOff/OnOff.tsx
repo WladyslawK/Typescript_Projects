1363326c9222d3a2e5c01e15c55cfeb25a5ff132
WladyslawK <vladkurr7890@gmail.com>
2022-09-02 23:10:57 +0200

    OnOff module

--- src/components/OnOff/OnOff.tsx
+++ src/components/OnOff/OnOff.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import s from "./OnOff.module.css"
 
 type OnOffType = {
@@ -6,13 +6,56 @@ type OnOffType = {
 }
 
 export const OnOff: React.FC<OnOffType> = ({value}) => {
-    return (
-        <div className={s.container}>
-            {value && <div className={`${s.container__On} ${s.container__indicator}`}>On</div>}
-            {!value && <div className={`${s.container__Off} ${s.container__indicator}`}>Off</div>}
 
-            {value && <div className={`${s.container__On} ${s.container__circleIndicator}`}></div>}
-            {!value && <div className={`${s.container__Off} ${s.container__circleIndicator}`}></div>}
+    const [indicatorValue, setIndicatorValue] = useState(value)
+
+    const changeIndicatorHandler = (newValue: boolean) => {
+        setIndicatorValue(newValue)
+    }
+
+    const containerStyle = {
+        display: "flex",
+        justifyContent: "center",
+        marginBottom: "5px",
+    }
+
+    const onContainer = {
+        display: "flex",
+        justifyContent: "center",
+        border: "1px solid black",
+        width: "50px",
+        height: "20px",
+        borderRadius: "6px",
+        padding: "2px",
+        margin: "2px",
+        backgroundColor: indicatorValue? "green" : "white",
+    }
+
+    const offContainer = {
+        display: "flex",
+        justifyContent: "center",
+        border: "1px solid black",
+        width: "50px",
+        height: "20px",
+        borderRadius: "6px",
+        padding: "2px",
+        margin: "2px",
+        backgroundColor: indicatorValue? "white" : "red",
+    }
+
+    const circleIndicator = {
+        width: "20px",
+        margin: "2px",
+        borderRadius: "50%",
+        border: "1px solid black",
+        backgroundColor: indicatorValue? "green" : "red",
+    }
+
+    return (
+        <div style={containerStyle}>
+            <div style={onContainer} onClick={() => changeIndicatorHandler(true)}>On</div>
+            <div style={offContainer} onClick={() => changeIndicatorHandler(false)}>Off</div>
+            <div style={circleIndicator}></div>
         </div>
     );
 };
