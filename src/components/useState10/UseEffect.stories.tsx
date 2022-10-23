ef4a042861ae8a20964629bf3d85cfef22632573
WladyslawK <vladkurr7890@gmail.com>
2022-10-23 23:37:39 +0200

    UseEffect vs SetTimeout and SetInterval

---
+++ src/components/useState10/UseEffect.stories.tsx
@@ -0,0 +1,69 @@
+import {useEffect, useState} from "react";
+
+export default {
+    title: "lesson10/UseEffect"
+}
+
+export const UseEffectCounterExample = () => {
+
+    const [counter, setCounter] = useState(0)
+    const [fakeCounter, setFakeCounter] = useState(0)
+
+    useEffect(()=> {
+        console.log("called only once (like componentDidMount in class Component)")
+    }, [])
+
+    useEffect(() => {
+        console.log("called only when dependency has been changed ([counter])")
+        document.title = counter.toString()
+    }, [counter])
+
+    useEffect(()=>{
+        console.log("called all the time (all useEffects called after jsx returned)")
+
+        //side effects
+        //api.getUsers().then('')
+        //setInterval
+        //indexedDB
+        //document.getElementById
+        //document.title = "User "
+    })
+
+
+    return (
+        <div>
+            <span>{counter}</span>
+            <button onClick={() => setCounter(counter + 1)}>Increment</button>
+            <br/>
+            <span>{fakeCounter}</span>
+            <button onClick={() => setFakeCounter(fakeCounter + 1)}>IncrFakeCounter</button>
+        </div>
+    )
+}
+
+export const UseEffectSetInterval = () => {
+
+    const [counter, setCounter] = useState(0)
+
+    useEffect(()=> {
+        setTimeout(()=> {
+            console.log("set title called")
+            document.title = counter.toString()
+        }, 1000)
+    }, [])
+
+    useEffect(()=> {
+        setInterval(()=> {
+            console.log("tick: ", counter)
+            setCounter(state => state + 1 )
+        }, 1000)
+    },[])
+
+
+    return(
+        <div>
+            <span>{counter}</span>
+            <button onClick={() => setCounter(counter + 1)}>Incr</button>
+        </div>
+    )
+}
